a706140aeac4c28287c5816954c86d9e4b222183
Sally Resch <sallyresch27@gmail.com>
2022-07-04 14:18:14 +0200

    boolean and char

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        char myChar = 'A';
+        char myUnicode = '\u0041';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        //16bits and 16 width = unicode = assigned number
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
     }
 }
